f1df2c9d10c0e15cf8879c67fa0424d7539adde8
raffaeleeliostasi-source <raffaeleeliostasi@gmail.com>
2026-09-10 13:39:03 +0200

    Update views.py

--- views.py
+++ views.py
@@ -8,9 +8,6 @@ import auth
 import database as db
 import pdf_generator
 
-intervento = db.get_intervento(tid)
-st.write("Dati intervento dal DB:", intervento)
-
 def is_admin():
     ruolo = str(st.session_state.get("ruolo", "")).strip().lower()
     return ruolo in ["amministratore", "admin"]
